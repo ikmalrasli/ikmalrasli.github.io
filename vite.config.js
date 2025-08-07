ed3bf6f17a0c123c7a1b58ccbecfa09efd4558e6
eemauuu <ikmalrasli@gmail.com>
2025-08-07 15:13:37 +0800

    fix: updated base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ikmalrasli.github.io/',
   plugins: [
     tailwindcss(),
     vue(),
