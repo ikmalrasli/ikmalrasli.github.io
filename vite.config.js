06a1b1b649cedc2a9bb8cd9c1c66550a74105997
eemauuu <ikmalrasli@gmail.com>
2025-08-07 15:23:09 +0800

    portfolio name change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ikmalrasli.github.io/',
+  base: '/portfolio/',
   plugins: [
     tailwindcss(),
     vue(),
